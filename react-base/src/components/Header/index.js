75784485c0e05532772719be5f66066b03c00bb8
Ivo Oliveira <ivo793@gmail.com>
2024-01-03 20:39:15 -0300

    .

--- react-base/src/components/Header/index.js
+++ react-base/src/components/Header/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import { Nav } from './styled';
 
 export default function Header() {
   return (
     <Nav>
-      <p>
+      <Link to="/">
         <FaHome size={24} />
-      </p>
-      <p>
+      </Link>
+      <Link to="/login">
         <FaUserAlt size={24} />
-      </p>
-      <p>
+      </Link>
+      <Link to="/asdasd">
         <FaSignInAlt size={24} />
-      </p>
+      </Link>
     </Nav>
   );
 }
--- react-base/src/routes/index.js
+++ react-base/src/routes/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="*" component={Page404} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="*" component={Page404} />
+    </Switch>
   );
 }
