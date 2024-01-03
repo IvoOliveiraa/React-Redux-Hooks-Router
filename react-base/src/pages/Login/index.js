e5981654fc67c7df2ebca7096c34d3369c8929a3
Ivo Oliveira <ivo793@gmail.com>
2024-01-03 17:03:49 -0300

    criando Styled-Components.

---
+++ react-base/src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>Login</Title>
+      <Paragrafo>Lorem5</Paragrafo>
+    </Container>
+  );
+}
---
+++ react-base/src/pages/Login/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1``;
+
+export const Paragrafo = styled.p`
+  font-size: 80px;
+`;
