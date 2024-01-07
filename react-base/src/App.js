2a086c73ed702e17f72b165a37c1594bde33055c
Ivo Oliveira <ivo793@gmail.com>
2024-01-06 21:53:18 -0300

    configurando o Redux Persist.

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import store, { persistor } from './store';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Login from './pages/Login';
@@ -12,15 +13,17 @@ import Page404 from './pages/Page404';
 export default function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-        <GlobalStyles />
-        <ToastContainer autoClose={3000} className="toast-container" />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} className="toast-container" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
